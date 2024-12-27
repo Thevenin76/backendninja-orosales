bb40f8800ec7bca9b5c4b210011f60faa4189d84
Thevenin76 <davidyanesfarina@gmail.com>
2024-12-27 18:44:34 +0000

    Se solucionan varios errores

--- src/main/java/com/udemy/backendninja/controller/LoginController.java
+++ src/main/java/com/udemy/backendninja/controller/LoginController.java
@@ -1,65 +1,40 @@
 package com.udemy.backendninja.controller;
 
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.stereotype.Controller;
 
-import com.udemy.backendninja.constant.ViewConstant;
+import org.springframework.data.authentication.UserCredentials;
 
 @Controller
+@RequestMapping("/")
 public class LoginController {
-	
 	private static final Log LOG = LogFactory.getLog(LoginController.class);
 
-	/*@GetMapping("/")
+	@GetMapping("/")
 	public String redirectToLogin() {
-		
-		LOG.info("Method: redirectToLogin() ");
 		return "redirect:/login";
-	}*/
-	
-	@GetMapping("/login") 
-	public String showLoginForm(Model model, 
-			@RequestParam(name="error",  required=false ) String error,
-			@RequestParam(name="logout",  required=false ) String logout) {
-		
-		LOG.info("Method: showLoginForm() -- PARAMS: error=" + error + "logout="+logout);
-		model.addAttribute("error", error);
-		model.addAttribute("logout", logout);
-		//model.addAttribute("userCredentials", new UserCredential());
-		LOG.info("returning to login view");
-		return ViewConstant.LOGIN;
+	}
+
+	@GetMapping("/login")
+	public String showLoginForm(Model model, @RequestParam(name="error", defaultValue="", required=false) String e) {
+		model.addAttribute("userCredentials", "UserCredentials userCredential");
+		return "login";
 	}
 	
-	/*@PostMapping("/logincheck") 
-	public String loginCheck(@ModelAttribute(name="userCredentials") UserCredential userCredential ) {
-		
+	@PostMapping("/logincheck")
+	public String loginCheck(@ModelAttribute(name="userCredentials") UserCredentials userCredential ) {
 		LOG.info("Method: loginCheck() -- PARAMS: " + userCredential.toString() );
-		
-		if ( userCredential.getUsername().equals("user") &&
-				userCredential.getPassword().equals("user")) {
-			LOG.info("returning to contact view");
-			//return ViewConstant.CONTACTS;	
-			return "redirect:/contacts/showcontacts";
-		} else {
-			LOG.info("redirect to login?error ");
-			return "redirect:/login?error";
+		if (userCredential.getUsername().equals("user") && userCredential.getPassword().equals("user")) {
+			return "contacts";
 		}
-		
-		
-	}*/
-	
-	@GetMapping( {"/loginsuccess", "/"}) 
-	public String loginCheck( ) {
-		
-		LOG.info("Method: loginCheck() " );
-		
-		return "redirect:/contacts/showcontacts";
-		
+		return "redirect:/login?error";
 	}
 	
 }
